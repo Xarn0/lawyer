4828b9e156a9b37dc52fb0272e7712ec6ac63004
xarn0 <xouli@list.ru>
2023-09-27 17:51:36 +0700

    all

---
+++ js/about.b0b778e7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkurls"]=self["webpackChunkurls"]||[]).push([[443],{5672:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var r=function(){var t=this,e=t._self._c;return e("section",[e("b-container",[e("b-breadcrumb",{attrs:{items:t.items}})],1)],1)},u=[],s={data(){return{items:[{text:"main",href:"/"},{text:"about",active:!0}]}}},a=s,i=n(1001),l=(0,i.Z)(a,r,u,!1,null,null,null),c=l.exports}}]);
+//# sourceMappingURL=about.b0b778e7.js.map
