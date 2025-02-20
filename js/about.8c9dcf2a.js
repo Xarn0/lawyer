1d37592fb8d9f30e2b77d2858550126e2372b324
xarn0 <xouli@list.ru>
2025-02-20 19:49:00 +0300

    Updates

---
+++ js/about.8c9dcf2a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkurls"]=self["webpackChunkurls"]||[]).push([[443],{1224:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var r=function(){var t=this,e=t._self._c;return e("section",[e("b-container",[e("b-breadcrumb",{attrs:{items:t.items}})],1)],1)},u=[],s={data(){return{items:[{text:"main",href:"/"},{text:"about",active:!0}]}}},a=s,i=n(1001),l=(0,i.Z)(a,r,u,!1,null,null,null),c=l.exports}}]);
+//# sourceMappingURL=about.8c9dcf2a.js.map
